fc6bbdee7bc80acfffb27c8c73499d130fe96905
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-06 10:42:31 +0200

    Add files via upload

---
+++ JavaCore 0441 Somehow mediocre/src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+/*
+
+Ввести с клавиатуры три числа, вывести на экран среднее из них.
+Т.е. не самое большое и не самое маленькое.
+Если все числа равны, вивести любое из них.
+
+Требования:
+1. Программа должна считывать числа c клавиатуры.
+2. Программа должна выводить число на экран.
+3. Программа должна выводить среднее из трех чисел.
+4. Если все числа равны, вывести любое из них.
+5. Если два числа из трех равны, вывести любое из двух.
+
+*/
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n1 = Integer.parseInt(reader.readLine());
+        int n2 = Integer.parseInt(reader.readLine());
+        int n3 = Integer.parseInt(reader.readLine());
+
+        if ((n1 >= n2) & (n1 >= n3))
+            System.out.println((n3 < n2) ? n2 : n3);
+
+        else if ((n2 >= n1) & (n2 >= n3))
+            System.out.println((n1 > n3) ? n1 : n3);
+
+        else if ((n3 >= n1) & (n3 >= n2))
+            System.out.println((n1 > n2) ? n1 : n2);
+
+    }
+}
+
+
+
